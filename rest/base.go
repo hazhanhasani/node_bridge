9800508adcbc76c7fa2df7e412acccd10db3e2c8
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-09-14 19:46:47 +0330

    refactor: use BluePanel Go module path

--- rest/base.go
+++ rest/base.go
@@ -13,9 +13,9 @@ import (
 
 	"google.golang.org/protobuf/proto"
 
-	"github.com/pasarguard/node_bridge/common"
-	"github.com/pasarguard/node_bridge/controller"
-	"github.com/pasarguard/node_bridge/tools"
+	"github.com/hazhanhasani/node_bridge/common"
+	"github.com/hazhanhasani/node_bridge/controller"
+	"github.com/hazhanhasani/node_bridge/tools"
 )
 
 type Node struct {
