226b94bbe3981eb11546cc14b4d820ef502720ae
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-09-14 20:02:31 +0330

    feat: add outbound latency to gRPC bridge

--- rpc/stats.go
+++ rpc/stats.go
@@ -10,59 +10,35 @@ import (
 func (n *Node) GetSystemStats() (*common.SystemStatsResponse, error) {
 	ctx, cancel := context.WithTimeout(n.ctx, 5*time.Second)
 	defer cancel()
-
-	resp, err := n.client.GetSystemStats(ctx, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
+	return n.client.GetSystemStats(ctx, nil)
 }
 
 func (n *Node) GetBackendStats() (*common.BackendStatsResponse, error) {
 	ctx, cancel := context.WithTimeout(n.ctx, 5*time.Second)
 	defer cancel()
-
-	resp, err := n.client.GetBackendStats(ctx, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
+	return n.client.GetBackendStats(ctx, nil)
 }
 
 func (n *Node) GetStats(reset bool, name string, statType common.StatType) (*common.StatResponse, error) {
 	ctx, cancel := context.WithTimeout(n.ctx, 5*time.Second)
 	defer cancel()
+	return n.client.GetStats(ctx, &common.StatRequest{Reset_: reset, Name: name, Type: statType})
+}
 
-	resp, err := n.client.GetStats(ctx, &common.StatRequest{Reset_: reset, Name: name, Type: statType})
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
+func (n *Node) GetOutboundsLatency(name string) (*common.LatencyResponse, error) {
+	ctx, cancel := context.WithTimeout(n.ctx, 10*time.Second)
+	defer cancel()
+	return n.client.GetOutboundsLatency(ctx, &common.LatencyRequest{Name: name})
 }
 
 func (n *Node) GetUserOnlineStat(email string) (*common.OnlineStatResponse, error) {
 	ctx, cancel := context.WithTimeout(n.ctx, 5*time.Second)
 	defer cancel()
-
-	resp, err := n.client.GetUserOnlineStats(ctx, &common.StatRequest{Name: email})
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
+	return n.client.GetUserOnlineStats(ctx, &common.StatRequest{Name: email})
 }
 
 func (n *Node) GetUserOnlineIpList(email string) (*common.StatsOnlineIpListResponse, error) {
 	ctx, cancel := context.WithTimeout(n.ctx, 5*time.Second)
 	defer cancel()
-
-	resp, err := n.client.GetUserOnlineIpListStats(ctx, &common.StatRequest{Name: email})
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
+	return n.client.GetUserOnlineIpListStats(ctx, &common.StatRequest{Name: email})
 }
