50a81e2cc0c9a851eebbb4b5af9a6edc01f4d24e
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-09-14 20:02:55 +0330

    feat: add outbound latency to REST bridge

--- rest/stats.go
+++ rest/stats.go
@@ -1,54 +1,55 @@
 package rest
 
 import (
+	"net/http"
+
 	"github.com/hazhanhasani/node_bridge/common"
 )
 
 func (n *Node) GetSystemStats() (*common.SystemStatsResponse, error) {
 	var stats common.SystemStatsResponse
-	err := n.createRequest(n.client, "GET", "stats/system", &common.Empty{}, &stats)
-	if err != nil {
+	if err := n.createRequest(n.client, http.MethodGet, "stats/system", &common.Empty{}, &stats); err != nil {
 		return nil, err
 	}
-
 	return &stats, nil
 }
 
 func (n *Node) GetBackendStats() (*common.BackendStatsResponse, error) {
 	var stats common.BackendStatsResponse
-	err := n.createRequest(n.client, "GET", "stats/backend", &common.Empty{}, &stats)
-	if err != nil {
+	if err := n.createRequest(n.client, http.MethodGet, "stats/backend", &common.Empty{}, &stats); err != nil {
 		return nil, err
 	}
-
 	return &stats, nil
 }
 
 func (n *Node) GetStats(reset bool, name string, statType common.StatType) (*common.StatResponse, error) {
 	var stats common.StatResponse
-	if err := n.createRequest(n.client, "GET", "stats", &common.StatRequest{Reset_: reset, Name: name, Type: statType}, &stats); err != nil {
+	if err := n.createRequest(n.client, http.MethodGet, "stats/", &common.StatRequest{Reset_: reset, Name: name, Type: statType}, &stats); err != nil {
 		return nil, err
 	}
-
 	return &stats, nil
+}
+
+func (n *Node) GetOutboundsLatency(name string) (*common.LatencyResponse, error) {
+	var latency common.LatencyResponse
+	if err := n.createRequest(n.client, http.MethodGet, "stats/latency", &common.LatencyRequest{Name: name}, &latency); err != nil {
+		return nil, err
+	}
+	return &latency, nil
 }
 
 func (n *Node) GetUserOnlineStat(email string) (*common.OnlineStatResponse, error) {
 	var stats common.OnlineStatResponse
-	err := n.createRequest(n.client, "GET", "stats/user/online", &common.StatRequest{Name: email}, &stats)
-	if err != nil {
+	if err := n.createRequest(n.client, http.MethodGet, "stats/user/online", &common.StatRequest{Name: email}, &stats); err != nil {
 		return nil, err
 	}
-
 	return &stats, nil
 }
 
 func (n *Node) GetUserOnlineIpList(email string) (*common.StatsOnlineIpListResponse, error) {
 	var stats common.StatsOnlineIpListResponse
-	err := n.createRequest(n.client, "GET", "stats/user/online_ip", &common.StatRequest{Name: email}, &stats)
-	if err != nil {
+	if err := n.createRequest(n.client, http.MethodGet, "stats/user/online_ip", &common.StatRequest{Name: email}, &stats); err != nil {
 		return nil, err
 	}
-
 	return &stats, nil
 }
